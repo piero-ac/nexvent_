41936e5755222bae2aea39b3baa2ba6d866d0957
Piero Coronado <pacoronado01@gmail.com>
2023-08-26 07:32:59 -0400

    Edit event properties to reference correct information

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,35 +1,43 @@
 /* eslint-disable react/prop-types */
 import classes from "../styling/EventItem.module.css";
+import { useNavigate } from "react-router-dom";
+import formatTimestamp from "../utils/formatTimestamp";
 
 const EventItem = ({ event }) => {
+	const navigate = useNavigate();
+
 	const title =
-		event.title.length > 30
-			? event.title.substring(0, 20) + "..."
-			: event.title;
+		event.event_name.length > 25
+			? event.event_name.substring(0, 20) + "..."
+			: event.event_name;
 
 	return (
 		<article className={classes.eventItem}>
 			<div className={classes.eventImage}>
 				<img
-					src={"https://picsum.photos/200"}
+					src={event.event_image}
 					width="150"
 					height="150"
 					alt={event.imgAlt}
 				/>
 			</div>
 			<div className={classes.eventInformation}>
-				<p>{event.date}</p>
+				<p>{formatTimestamp(event.event_date)}</p>
 				<p>
 					<span className={classes.eventTitle}>{title}</span>
 				</p>
 				<p>
-					<span className={classes.eventLocation}>{event.location}</span>
+					<span
+						className={classes.eventLocation}
+					>{`${event.address.city.stringValue}, ${event.address.state.stringValue}`}</span>
 				</p>
-				<p>By {event.organizer}</p>
-				<p>{event.attendees} Attendees</p>
+				<p>By TBD</p>
+				<p>{event.number_of_attendees} Attendees</p>
 			</div>
 			<div className={classes.eventNavigate}>
-				<button>Let&apos;s Go</button>
+				<button onClick={() => navigate(`/event/${event.document_id}`)}>
+					Let&apos;s Go
+				</button>
 			</div>
 		</article>
 	);
