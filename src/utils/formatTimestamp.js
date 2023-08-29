e202874e867ad89a6c7150523b8e21c0bdec787b
Piero Coronado <pacoronado01@gmail.com>
2023-08-28 20:26:30 -0400

    Add time conversion from timestamp

--- src/utils/formatTimestamp.js
+++ src/utils/formatTimestamp.js
@@ -21,5 +21,21 @@ export default function formatTimestamp(timestamp) {
 	const month = months[date.getUTCMonth()];
 	const day = date.getUTCDate();
 
-	return `${dayOfWeek}, ${month} ${day}`;
+	// Get local time components
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+
+	// Determine whether it's AM or PM
+	const amOrPm = hours >= 12 ? "PM" : "AM";
+
+	// Convert to 12-hour format
+	const hours12 = hours % 12 || 12;
+
+	// Format minutes with leading zero
+	const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+	return {
+		date: `${dayOfWeek}, ${month} ${day}`,
+		time: `${hours12}:${formattedMinutes} ${amOrPm}`,
+	};
 }
