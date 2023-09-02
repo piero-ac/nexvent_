abedc0125edf37f4402973a3fb8dfd59bd9a162e
Piero Coronado <pacoronado01@gmail.com>
2023-09-02 15:17:03 -0400

    Create exported variables for database and collections

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -4,7 +4,9 @@ const client = new Client()
 	.setEndpoint(import.meta.env.VITE_APPWRITE_SERVER)
 	.setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-export const db = new Databases(Client);
+export const NEXVENT_DB_ID = "64f10ba63cc6da0ecf53";
+export const NEXVENT_EVENTS_COL_ID = "64f256d14f42950aedb6";
+export const db = new Databases(client);
 
 export const account = new Account(client);
 
