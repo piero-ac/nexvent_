c6083f2ee978e92186e48eac1619d8516ca8c77a
Piero Coronado <pacoronado01@gmail.com>
2023-09-03 15:58:09 -0400

    Import Storage utility

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,4 +1,4 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client()
 	.setEndpoint(import.meta.env.VITE_APPWRITE_SERVER)
@@ -6,8 +6,9 @@ const client = new Client()
 
 export const NEXVENT_DB_ID = "64f10ba63cc6da0ecf53";
 export const NEXVENT_EVENTS_COL_ID = "64f256d14f42950aedb6";
+export const NEXVENT_BUCKET_ID = "64f4d34b15ae78bb3191";
 export const db = new Databases(client);
-
+export const storage = new Storage(client);
 export const account = new Account(client);
 
 export default client;
