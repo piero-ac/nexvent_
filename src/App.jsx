3ca2d338a70e5202dd72c403f84f5bfbd1776ad1
Piero Coronado <pacoronado01@gmail.com>
2023-09-05 19:53:15 -0400

    Create route for viewing event information

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+	createBrowserRouter,
+	redirect,
+	RouterProvider,
+} from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import Home from "./pages/HomePage";
 import Login from "./pages/LoginPage";
 import Profile from "./pages/ProfilePage";
+import EventPage from "./pages/EventPage";
 import PrivateRoutes from "./pages/PrivateRoutesLayout";
 import NewEvent from "./pages/NewEventPage";
 
@@ -28,6 +33,13 @@ const router = createBrowserRouter([
 				path: "new",
 				element: <PrivateRoutes />,
 				children: [{ index: true, element: <NewEvent /> }],
+			},
+			{
+				path: "event/:eventId",
+				// loader: () => {
+				// 	return redirect("/new");
+				// },
+				element: <EventPage />,
 			},
 		],
 	},
