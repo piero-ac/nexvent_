4726623c2f734a54aafe80b537e3b06235acbfef
Piero Coronado <pacoronado01@gmail.com>
2023-09-05 19:52:43 -0400

    Add reference to attendees collection

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -7,6 +7,7 @@ const client = new Client()
 export const NEXVENT_DB_ID = "64f10ba63cc6da0ecf53";
 export const NEXVENT_EVENTS_COL_ID = "64f256d14f42950aedb6";
 export const NEXVENT_BUCKET_ID = "64f4d34b15ae78bb3191";
+export const NEXVENT_EVENT_ATTENDEES_COL_ID = "64f64a7a9f98c6d2cc31";
 export const db = new Databases(client);
 export const storage = new Storage(client);
 export const account = new Account(client);
