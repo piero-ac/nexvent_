d4a7f1aa7e9f4d630c0016c92d0672bc2aaff3af
Piero Coronado <pacoronado01@gmail.com>
2023-09-13 19:33:50 -0400

    Create loader function for fetching all events

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -2,6 +2,8 @@ import { json, useLoaderData } from "react-router-dom";
 import classes from "../styling/EventsPage.module.css";
 import FiltersForm from "../components/FiltersForm";
 import EventItem from "../components/EventItem";
+import { db, NEXVENT_DB_ID, NEXVENT_EVENTS_COL_ID } from "../appwriteConfig";
+import { Query } from "appwrite";
 
 const Events = () => {
 	const data = useLoaderData();
@@ -9,12 +11,12 @@ const Events = () => {
 	return (
 		<main className={classes.main}>
 			<h1>
-				<span>{data.events.length}</span> events waiting for you!
+				<span>{data.total}</span> events waiting for you!
 			</h1>
 			<FiltersForm />
 			<section className={classes.events}>
-				{data.events.map((event) => (
-					<EventItem event={event} key={event.document_id} />
+				{data.documents.map((event) => (
+					<EventItem event={event} key={event.$id} />
 				))}
 			</section>
 		</main>
@@ -22,12 +24,13 @@ const Events = () => {
 };
 
 export async function loader() {
-	const response = await fetch("http://localhost:3000/events");
-	if (!response.ok) {
-		throw json({ message: "Could not fetch events." }, { status: 500 });
-	} else {
-		return response;
-	}
+	const response = await db.listDocuments(
+		NEXVENT_DB_ID,
+		NEXVENT_EVENTS_COL_ID,
+		[Query.limit(10), Query.orderDesc("eventDateTime")]
+	);
+
+	return json(response);
 }
 
 export default Events;
