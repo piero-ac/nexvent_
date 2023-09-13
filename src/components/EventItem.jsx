7c615e8686cd4fa44d77b99168a6d1174255d2ed
Piero Coronado <pacoronado01@gmail.com>
2023-09-13 19:32:41 -0400

    Edit EventItem component to access correct event properties

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,44 +1,53 @@
 /* eslint-disable react/prop-types */
 import classes from "../styling/EventItem.module.css";
 import { useNavigate } from "react-router-dom";
-import formatTimestamp from "../utils/formatTimestamp";
+import { NEXVENT_BUCKET_ID, storage } from "../appwriteConfig";
+import { useEffect, useState } from "react";
+import { formatISO1086 } from "../utils/formatISO1086";
 
 const EventItem = ({ event }) => {
+	const [imageUrl, setImageUrl] = useState(null);
 	const navigate = useNavigate();
-	const { date } = formatTimestamp(event.event_date);
 
 	const title =
-		event.event_name.length > 25
-			? event.event_name.substring(0, 20) + "..."
-			: event.event_name;
+		event.eventTitle.length > 25
+			? event.eventTitle.substring(0, 20) + "..."
+			: event.eventTitle;
+
+	const location =
+		event.eventType === "online"
+			? "Online"
+			: event.eventLocation.substring(0, 20) + "...";
+
+	useEffect(() => {
+		const fetchEventImage = async () => {
+			const file = await storage.getFileView(
+				NEXVENT_BUCKET_ID,
+				event.eventImage
+			);
+			setImageUrl(file.href);
+		};
+		fetchEventImage();
+	}, []);
 
 	return (
 		<article className={classes.eventItem}>
 			<div className={classes.eventImage}>
-				<img
-					src={event.event_image}
-					width="150"
-					height="150"
-					alt={event.imgAlt}
-				/>
+				<img src={imageUrl} width="150" height="150" alt="Event Image" />
 			</div>
 			<div className={classes.eventInformation}>
-				<p>{date}</p>
+				<p>{formatISO1086(event.eventDateTime, "full")}</p>
 				<p>
 					<span className={classes.eventTitle}>{title}</span>
 				</p>
 				<p>
-					<span
-						className={classes.eventLocation}
-					>{`${event.address.city.stringValue}, ${event.address.state.stringValue}`}</span>
+					<span className={classes.eventLocation}>{location}</span>
 				</p>
-				<p>By TBD</p>
-				<p>{event.number_of_attendees} Attendees</p>
+				<p>By {event.eventCreatorName}</p>
+				<p>1 Attendees</p>
 			</div>
 			<div className={classes.eventNavigate}>
-				<button onClick={() => navigate(`${event.document_id}`)}>
-					Let&apos;s Go
-				</button>
+				<button onClick={() => navigate(`${event.$id}`)}>Let&apos;s Go</button>
 			</div>
 		</article>
 	);
